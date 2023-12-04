35dbb67cc841b989a940466d12e5df478ec19fff
norhan youser <norhan.yousery96@gmail.com>
2023-12-04 23:08:06 +0200

    adding new files

---
+++ calc.classlibrary/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace calc.classlibrary
+{
+    public class calc1
+    {
+
+
+
+
+        public int Average(int[] nums)
+        {
+            int total = 0;
+            foreach (var num in nums)
+            {
+                total += num;
+            }
+            return total / nums.Count();
+
+        }
+        public int Smallest(int[] nums)
+        {
+            int small = nums[0];
+            foreach (var num in nums)
+            {
+                if (num < small)
+                {
+                    small = num;
+                }
+            }
+            return small;
+        }
+        public int Largest(int[] nums)
+        {
+            int large = nums[0];
+            foreach (var num in nums)
+            {
+                if (num > large)
+                {
+                    large = num;
+
+                }
+
+            }
+            return large;
+
+        }
+    }
+}
